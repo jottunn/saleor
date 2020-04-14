dd18ba692c4373b72f76441fba357294347ca74e
jotun <ygs_Geografie1#>
2020-04-14 13:49:22 +0300

    -added ckeditor to page content field

--- saleor/page/models.py
+++ saleor/page/models.py
@@ -1,9 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django.utils.translation import pgettext_lazy
 from draftjs_sanitizer import clean_draft_js
+from ckeditor_uploader.fields import RichTextUploadingField
 
 from ..core.db.fields import SanitizedJSONField
 from ..core.models import PublishableModel, PublishedQuerySet
-from ..core.permissions import PagePermissions
+from ..core.utils import build_absolute_uri
+from ..core.utils.draftjs import json_content_to_raw_text
 from ..core.utils.translations import TranslationProxy
 from ..seo.models import SeoModel, SeoModelTranslation
 
@@ -11,13 +17,13 @@ from ..seo.models import SeoModel, SeoModelTranslation
 class PagePublishedQuerySet(PublishedQuerySet):
     @staticmethod
     def user_has_access_to_all(user):
-        return user.is_active and user.has_perm(PagePermissions.MANAGE_PAGES)
+        return user.is_active and user.has_perm("page.manage_pages")
 
 
 class Page(SeoModel, PublishableModel):
-    slug = models.SlugField(unique=True, max_length=255)
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, max_length=100)
+    title = models.CharField(max_length=200)
+    content = RichTextUploadingField(blank=True)
     content_json = SanitizedJSONField(
         blank=True, default=dict, sanitizer=clean_draft_js
     )
@@ -28,10 +34,24 @@ class Page(SeoModel, PublishableModel):
 
     class Meta:
         ordering = ("slug",)
-        permissions = ((PagePermissions.MANAGE_PAGES.codename, "Manage pages."),)
+        permissions = (
+            ("manage_pages", pgettext_lazy("Permission description", "Manage pages.")),
+        )
 
     def __str__(self):
         return self.title
+
+    @property
+    def plain_text_content(self):
+        if settings.USE_JSON_CONTENT:
+            return json_content_to_raw_text(self.content_json)
+        return strip_tags(self.content)
+
+    def get_absolute_url(self):
+        return reverse("page:details", kwargs={"slug": self.slug})
+
+    def get_full_url(self):
+        return build_absolute_uri(self.get_absolute_url())
 
 
 class PageTranslation(SeoModelTranslation):
