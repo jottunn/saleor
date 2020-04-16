dc0dedfd9982707ee5db3414332a82f807e3b929
jotun <ygs_Geografie1#>
2020-04-16 10:53:21 +0300

    -git issues
     Conflicts:
    	Dockerfile

--- saleor/discount/error_codes.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class DiscountErrorCode(Enum):
-    ALREADY_EXISTS = "already_exists"
-    INVALID = "invalid"
-    NOT_FOUND = "not_found"
-    REQUIRED = "required"
-    UNIQUE = "unique"
